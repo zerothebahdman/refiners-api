f87c5af3db7d276b864950c062599ab796e2a76d
Divinewill Okoro <zerothebahdman@users.noreply.github.com>
2022-09-26 13:43:08 +0100

    Chore: refactored user route and controller

--- src/interface/http/express/controllers/users.controller.ts
+++ src/interface/http/express/controllers/users.controller.ts
@@ -1,15 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import UserService from '../../../../services/User.service';
 import AppException from '../../../../exceptions/AppException';
 
-import { PrismaClient } from '@prisma/client';
-const { user } = new PrismaClient();
-
 export default class UserController {
-  async getAllUsers(req: Request, res: Response, next: NextFunction) {
+  async getAllUsers(_req: Request, res: Response, next: NextFunction) {
     try {
-      const data = await UserService.getAllUsers();
-      return res.status(200).json({ status: 'success', data });
+      return res.status(200).json({ status: 'success' });
     } catch (err: any) {
       return next(new AppException(err.message, err.status));
     }

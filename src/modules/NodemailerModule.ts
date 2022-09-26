843002e5654ab80295d36dfad93113bd88209b87
Divinewill Okoro <zerothebahdman@users.noreply.github.com>
2022-09-26 14:12:43 +0100

    Feature: changed the structure of nodemailer

--- src/modules/NodemailerModule.ts
+++ src/modules/NodemailerModule.ts
@@ -1,13 +1,13 @@
 import nodemailer, { Transporter } from 'nodemailer';
-import config from 'config';
+import config from '../../config/default';
 import log from '../logging/logger';
 
-type MailData = {
+export type MailData = {
   from: string;
   to: string;
-  html: string;
-  name: string;
-  subject: string;
+  html?: string;
+  name?: string;
+  subject?: string;
 };
 
 export default class NodemailerModule {
@@ -15,22 +15,34 @@ export default class NodemailerModule {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: config.get<string>('MAIL_HOST'),
-      port: config.get<number>('MAIL_PORT'),
+      host: config.MAIL_HOST,
+      port: Number(config.MAIL_PORT),
+      connectionTimeout: 300000,
+      pool: true,
       logger: true,
       secure: false,
       auth: {
-        user: config.get<string>('MAIL_USER'),
-        pass: config.get<string>('MAIL_PASSWORD'),
+        user: config.MAIL_USER,
+        pass: config.MAIL_PASSWORD,
       },
-      ignoreTLS: true,
+      ignoreTLS: false,
     });
   }
 
   async send(mailData: MailData) {
-    return this.transporter.sendMail(mailData, (err, data) => {
-      if (err) log.error(err);
-      else log.info(`Email sent successfully ${data}`);
+    this.transporter.verify((error) => {
+      if (error) log.error(`Error sending email :::::: ${error}`);
+      else log.info('Server 🚀 is ready to send out mails');
+    });
+
+    return this.transporter.sendMail(mailData, (error, info) => {
+      if (error) log.error(`Error sending email :::::: ${error}`);
+      else {
+        console.log(`Email sent: ${info.response}`);
+        console.log(`Email sent to: ${info.accepted}`);
+        console.log(`Email sent count: ${info.accepted?.length}`);
+        console.log(`Email failed Count: ${info.rejected?.length}`);
+      }
     });
   }
 }

3911e2af0a5177c95b439a2cac6ce39c048085f0
Divinewill Okoro <zerothebahdman@users.noreply.github.com>
2022-10-08 12:17:28 +0100

    Feature: refactored authentication flow

--- src/authentication/LoginUser.ts
+++ src/authentication/LoginUser.ts
@@ -5,16 +5,45 @@ import AuthService from '../services/Auth.service';
 
 export default class LoginUser {
   constructor(private readonly authService: AuthService) {}
+
   async _loginUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const token = await this.authService.loginUser(req.body, next);
-      res.status(httpStatus.OK).json({
+      const { accessToken, refreshToken, user } =
+        await this.authService.loginUser(req.body, next);
+      return res.status(httpStatus.ACCEPTED).json({
         status: 'success',
-        message: 'Login successful',
-        token,
+        user,
+        accessToken,
+        refreshToken,
       });
     } catch (err: any) {
       return next(new AppException(err.message, err.status));
     }
   }
+
+  async regenerateAccessToken(req: Request, res: Response, next: NextFunction) {
+    try {
+      const accessToken = await this.authService.regenerateAccessToken(
+        req.body.refreshToken,
+        next
+      );
+      if (!accessToken || accessToken.trim() === '')
+        return next(
+          new AppException('Oops! Refresh token expired.', httpStatus.FORBIDDEN)
+        );
+
+      return res.status(httpStatus.OK).json({ status: 'success', accessToken });
+    } catch (err: any) {
+      return next(new AppException(err.message, err.status));
+    }
+  }
+
+  // async resendOtp(req: Request, res: Response, next: NextFunction) {
+  //   try {
+  //     await this.authService.resendOtp({ req: req.body, next });
+  //     return res.status(httpStatus.NO_CONTENT).send();
+  //   } catch (err: any) {
+  //     return next(new AppException(err.message, err.status));
+  //   }
+  // }
 }

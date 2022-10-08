83cef7200aa5741a8b4efdbd78da4586becb7bbe
Divinewill Okoro <zerothebahdman@users.noreply.github.com>
2022-10-08 12:20:40 +0100

    Chore: added more npm module's

--- src/interface/http/express/app.ts
+++ src/interface/http/express/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import config from 'config';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 import enforce from 'express-sslify';
-import userRouter from './router/user/user.route';
 import { ErrorHandler } from './middlewares/error_handler.middleware';
 import AppException from '../../../exceptions/AppException';
 import morgan from 'morgan';
+import router from './router/v1/router.module';
 
 const app: Application = express();
 
@@ -17,11 +21,26 @@ if (config.get<string>('env') === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+app.use(express.json({ limit: '2MB' }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(hpp());
+app.use(helmet());
+app.use(mongoSanitize());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  skipSuccessfulRequests: true,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 app.disable('x-powered-by');
 
-app.use('/api/v1/user', userRouter);
+app.get('/', (_req, res) => {
+  res.send('<b>Welcome to your App!</b>');
+});
+
+app.use('/api/v1', router);
 
 app.all('*', (req: Request, _res: Response, next: NextFunction) => {
   return next(
@@ -30,13 +49,3 @@ app.all('*', (req: Request, _res: Response, next: NextFunction) => {
 });
 app.use(ErrorHandler);
 export default app;
-
-// class App {
-//   public app: Application;
-//   public port: number;
-
-//   constructor() {
-//     this.app = express();
-//     this.port = config.get<number>('port');
-//   };
-// }

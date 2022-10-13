374f34e393b5efa80c1ac7d03702721536ab835f
Divinewill Okoro <zerothebahdman@users.noreply.github.com>
2022-10-13 15:27:06 +0100

    Feature: implemented transaction model

--- src/database/models/transaction.model.ts
+++ src/database/models/transaction.model.ts
@@ -1,14 +1,16 @@
 import { Schema, model } from 'mongoose';
 import auditableFields from '../plugins/auditableFields.plugin';
+import { TRANSACTION_TYPE } from '../../utils/constants';
 
 const TransactionSchema = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: 'User' },
-    transactionType: String,
+    transactionType: { type: String, enum: Object.values(TRANSACTION_TYPE) },
     amount: String,
-    narration: String,
+    description: String,
     account: String,
-    date: Date,
+    date: String,
+    balance: Number,
     ...auditableFields,
   },
   { timestamps: true }

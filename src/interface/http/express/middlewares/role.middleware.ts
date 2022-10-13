d39a44229b7131e7ec3c18dd85d51b0eeae38bff
Divinewill Okoro <zerothebahdman@users.noreply.github.com>
2022-10-13 15:34:28 +0100

    Feature: Implemented role middleware handler

---
+++ src/interface/http/express/middlewares/role.middleware.ts
@@ -0,0 +1,21 @@
+import AppException from '../../../../exceptions/AppException';
+import { NextFunction, Request, Response } from 'express';
+
+export type RequestType = {
+  [prop: string]: any;
+} & Request;
+
+export const restrictAccessTo =
+  (...roles: string[]) =>
+  (req: RequestType, _res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppException(
+          `Oops! you don't have the privilege to perform this action`,
+          403
+        )
+      );
+    }
+
+    next();
+  };

b72de276b16c04a602fa0d7762f3ba9ccd4606b6
Divinewill Zero <zerothebahdman@users.noreply.github.com>
2022-10-31 10:13:08 +0100

    Refactored pagination for api endpoint

--- src/services/Transaction.service.ts
+++ src/services/Transaction.service.ts
@@ -14,32 +14,56 @@ export default class TransactionService {
     const limit = Number(filterOptions.limit) || 10;
     const skip = (page - 1) * limit;
     let transactions;
-
+    let totalNumberOfTransactions;
     if (filterOptions.allTransactions) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+        });
     } else if (filterOptions.thriftSavings) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user, account: 'thriftSavings' })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'thriftSavings',
+        });
     } else if (filterOptions.shareCapital) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user, account: 'shareCapital' })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'shareCapital',
+        });
     } else if (filterOptions.fine) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user, account: 'fine' })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'fine',
+        });
     } else if (filterOptions.loan) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user, account: 'loan' })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'loan',
+        });
     } else if (filterOptions.projectFinancing) {
       transactions = await this.transactionRepository
         .find({
@@ -48,19 +72,34 @@ export default class TransactionService {
         })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'projectFinancing',
+        });
     } else if (filterOptions.specialDeposit) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user, account: 'specialDeposit' })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'specialDeposit',
+        });
     } else if (filterOptions.commodityTrading) {
       transactions = await this.transactionRepository
         .find({ user: filterOptions.user, account: 'commodityTrading' })
         .skip(skip)
         .limit(limit);
+      totalNumberOfTransactions =
+        await this.transactionRepository.countDocuments({
+          user: filterOptions.user,
+          account: 'commodityTrading',
+        });
     }
     const account = await Account.findOne({ user: filterOptions.user });
     account.toJSON;
-    return { transactions, page, limit, account };
+    return { transactions, page, limit, account, totalNumberOfTransactions };
   }
 }
